3ad0b37566914bed83fd49ca98c9260c850408bf
Heisjabo <jabojeanmarie5@gmail.com>
2024-04-10 21:22:42 +0200

    Add middlewares for user authentication and ownership validation.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,11 +2,13 @@ import { getUsers } from "../controllers/userController";
 import express from "express";
 import { updateUserProfile, getUserById } from "../controllers/userController";
 import upload from "../middlewares/multer";
+import { isLoggedIn } from "../middlewares/isLoggedIn";
+import { isProfileOwner } from "../middlewares/isProfileOwner";
 
 const userRouter = express.Router();
 
 userRouter.get('/', getUsers);
 userRouter.get('/:id', getUserById);
-userRouter.patch('/:id', upload.single("profileImage"), updateUserProfile);
+userRouter.patch('/:id', isLoggedIn, isProfileOwner, upload.single("profileImage"), updateUserProfile);
 
 export default userRouter;
